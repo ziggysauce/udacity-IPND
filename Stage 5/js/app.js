6356886eef92ed7bf567501d18fe49411b8230a9
Dan Nguyen <dtn3423@gmail.com>
2017-09-19 12:50:29 -0400

    cards flip and change when matched

--- Stage 5/js/app.js
+++ Stage 5/js/app.js
@@ -1,6 +1,5 @@
 /* JS File */
 
-var array = ['fb', 'gplus', 'ig', 'pinterest', 'tumblr', 'reddit', 'snap', 'twitter'];
 
 /*
  * Display the cards on the page
@@ -37,23 +36,69 @@ var array = ['fb', 'gplus', 'ig', 'pinterest', 'tumblr', 'reddit', 'snap', 'twit
  */
 
 window.onload = function() {
-  show();
-}
+  flipCards();
+};
 
-var currentPair = [];
+var cardId = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+var currentPair = []; var storeId = []; var clickNum = 0;
 
 /**
  * Shows contents of card when clicked on
  * Temporarily stores clicked cards in array to find match
  * If two cards match, they stay showing and change color
- * If two cards do not match, they flip back and click resets
+ * If two cards do not match, they flip back and click reset
  */
-function show() {
-  var currentCard = document.getElementById('card-one');
-    currentCard.onclick = function() {
-    this.classList.toggle('show');
-    this.classList.toggle('open');
-    currentPair.push(this.className);
-    console.log(currentPair);
+function flipCards() {
+  let doThis = cardId.map(function(i) {
+    let clickMe = document.getElementById('card' + i);
+    clickMe.onclick = function() {
+      this.classList.toggle('show');
+      this.classList.toggle('open');
+      clickNum++;
+      storeId.push(i);
+      currentPair.push(this.className);
+      // pairs();
+      console.log(currentPair);
+      console.log(storeId);
+      if (clickNum == 2) {
+        checkCards();
+      }
+    };
+  });
+};
+
+/**
+ * Compares classes of two cards to see if they match
+ * If match, add match class and have cards stay flipped
+ * If no match, reset array and re-flip card to back
+ */
+function checkCards() {
+  // Cards match
+  if (currentPair[0] == currentPair[1]) {
+      // Add match class to cards
+      document.getElementById('card' + storeId[0]).classList.add('match');
+      document.getElementById('card' + storeId[1]).classList.add('match');
+      console.log(this.className);
+      console.log("match");
+      currentPair = [];
+      storeId = [];
+      clickNum = 0;
+    }
+  // Cards don't match. Reset array, flip cards back
+  else if (currentPair[0] !== currentPair[1]) {
+    document.getElementById('card' + storeId[0]).classList.remove('show', 'open');
+    document.getElementById('card' + storeId[1]).classList.remove('show', 'open');
+    currentPair = [];
+    storeId = [];
+    clickNum = 0;
+    console.log("no match");
   };
-}
+};
+
+
+// function pairs() {
+//   let getPairs = cardId.map(function(j) {
+//     let showMe = document.getElementById('card' + j);
+//     console.log(showMe.classList);
+//   })
+// }
